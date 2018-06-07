9ee31cdddca3b2c210beab7206e8fb4ee89e4229
Pavel Bambollo <webkupas@gmail.com>
2018-06-07 19:18:57 +0300

    Starter commit

---
+++ playground/mongodb-connect.js
@@ -0,0 +1,33 @@
+//const MongoClient = require('mongodb').MongoClient
+const {MongoClient, ObjectID} = require('mongodb')
+
+var obj = new ObjectID()
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
+  if (err) return console.error('Unable to connect to MongoDB Server')
+  console.log('Connected to MongoDB Server')
+  const db = client.db('TodoApp')
+
+  // db.collection('Todos').insertOne({
+  //   text: 'Hello there!',
+  //   completed: false
+  // }, (err, result) => {
+  //   if (err) return console.log('Unable to insert todo item:', err)
+  //   console.log(JSON.stringify(result.ops, undefined, 2))   
+  // })
+
+  // db.collection('Users').insertOne({
+  //   name: 'Nicky',
+  //   location: 'Ukraine',
+  //   age: 3
+  // }, (err, result) => {
+  //   if (err) return console.error('Unable to add new user in DB:', err)
+  //   console.log('New user successfully added!')
+  //   console.log(JSON.stringify(result.ops, undefined, 2))
+  // })
+
+  db.collection('Users').find({location: 'Ukraine'}).toArray().then(doc => {
+    console.log(JSON.stringify(doc, undefined, 2))
+  })
+  client.close()
+})
